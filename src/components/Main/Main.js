f0e41183999ab367733dbc013d62ec19a50d8dd5
titoworlddev <titocristian.dev@gmail.com>
2023-01-20 11:52:44 +0100

    Used getExercises correctly to show exercises in main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.css';
 import { getExercises } from '../../services/getExercises';
 
-const example = {
-  bodyPart: 'waist',
-  equipment: 'body weight',
-  gifUrl: 'http://d205bpvrqc9yn1.cloudfront.net/0001.gif',
-  id: '0001',
-  name: '3/4 sit-up',
-  target: 'abs'
-};
-
 export default function Main() {
-  let exercises = [];
-  getExercises().then(data => (exercises = data));
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    if (exercises.length === 0)
+      getExercises('/target/biceps').then(data => setExercises(data));
+  }, [exercises]);
 
   return (
     <main className="app-main">
       <section className="exercises-container">
-        {getExercises().then(() =>
-          exercises.map(exercise => (
-            <div key={exercise.id} className="exercise-card">
-              <p>{example.name}</p>
-              <img src={example.gifUrl} alt={`${example.name} gif`} />
-              <p>{`Target: ${example.target}`}</p>
-              <p>{`Equipment: ${example.equipment}`}</p>
-            </div>
-          ))
-        )}
+        {exercises.map((exercise, index) => (
+          <div key={index} className="exercise-card">
+            <img src={exercise.gifUrl} alt={`${exercise.name} gif`} />
+            <p>Name: {exercise.name}</p>
+            <p>Body part: {exercise.bodyPart}</p>
+            <p>Equipment: {exercise.equipment}</p>
+            <p>Target: {exercise.target}</p>
+          </div>
+        ))}
       </section>
     </main>
   );
