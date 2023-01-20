4e735e3c1d6c31a344a5187b1067afa3a31134d1
titoworlddev <titocristian.dev@gmail.com>
2023-01-20 22:40:29 +0100

    Add home intro

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,6 +5,21 @@ export default function HomePage() {
   return (
     <>
       <Header />
+      <main className="home-main">
+        <h2>Dou you want to create your own exercises?</h2>
+        <br />
+        <p>
+          Are you the type of person who likes to train in his own way?
+          <br /> <br />
+          Have you always wanted to create your workouts but didn't have where?
+          <br /> <br />
+          You can do it here. Just click the button below and start creating
+          your own exercises with the <strong>Workout Creator.</strong>
+        </p>
+        <button className="app-button-primary app-button-primary-big">
+          Start creating
+        </button>
+      </main>
     </>
   );
 }
