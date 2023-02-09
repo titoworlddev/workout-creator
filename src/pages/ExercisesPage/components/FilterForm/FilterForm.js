cf1ef6273c20d12f934c86dd7b156a47b7f820ef
titoworlddev <titocristian.dev@gmail.com>
2023-02-09 09:48:45 +0100

    Optimized handle Functions

--- src/pages/ExercisesPage/components/FilterForm/FilterForm.js
+++ src/pages/ExercisesPage/components/FilterForm/FilterForm.js
@@ -16,15 +16,9 @@ export default function FilterForm({
     form.querySelector('#bodypart-filter').value = 'back';
   }, []);
 
-  function preventFormSubmit() {
-    document
-      .querySelector('#filter-form')
-      .addEventListener('submit', e => e.preventDefault());
-  }
-
-  function handleFilterExercises() {
-    preventFormSubmit();
+  function handleFormSubmit() {
     const form = document.querySelector('#filter-form');
+    form.addEventListener('submit', e => e.preventDefault());
 
     setBodyPart(form.querySelector('#bodypart-filter').value);
     setTarget(form.querySelector('#target-filter').value);
@@ -32,20 +26,15 @@ export default function FilterForm({
   }
 
   function handleResetFilters() {
-    preventFormSubmit();
     const form = document.querySelector('#filter-form');
 
     form.querySelector('#bodypart-filter').value = 'back';
     form.querySelector('#target-filter').value = '- Select -';
     form.querySelector('#equipment-filter').value = '- Select -';
-
-    setBodyPart('back');
-    setTarget('');
-    setEquipment('');
   }
 
   return (
-    <form id="filter-form">
+    <form id="filter-form" onSubmit={handleFormSubmit}>
       <FilterLabel name="Body part" array={PARTS} id="bodypart-filter" />
       <FilterLabel name="Target" array={TARGETS} id="target-filter" />
       <FilterLabel name="Equipment" array={EQUIPMENTS} id="equipment-filter" />
@@ -53,17 +42,11 @@ export default function FilterForm({
       <div className="form-buttons-container">
         <button
           className="form-submit app-btn-primary"
-          id="form-submit"
           onClick={handleResetFilters}>
           🔄️
         </button>
 
-        <button
-          className="form-submit app-btn-primary"
-          id="form-submit"
-          onClick={handleFilterExercises}>
-          Filter
-        </button>
+        <button className="form-submit app-btn-primary">Filter</button>
       </div>
 
       <h3 className="exercises-number">Showing 1342 exercises</h3>
