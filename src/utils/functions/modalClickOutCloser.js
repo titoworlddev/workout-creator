263419b4a0a2e5cb8e10fdcba9926e2947cdc10a
titoworlddev <titocristian.dev@gmail.com>
2023-02-13 16:55:50 +0100

    Moved modalClickOutCloser

--- src/utils/functions/modalClickOutCloser.js
+++ src/utils/functions/modalClickOutCloser.js
@@ -1,12 +1,17 @@
 export function modalClickOutCloser() {
+  window.removeEventListener('click', () => {});
+
   const dayModal = document.querySelector('#day-modal');
   const exerciseModal = document.querySelector('#exercise-modal');
+  const chooseExerciseModal = document.querySelector('#choose-exercise-modal');
+
   const modalChildren = [
     ...Array.from(dayModal.querySelectorAll('*')),
     ...Array.from(exerciseModal.querySelectorAll('*'))
   ];
   window.addEventListener('click', event => {
-    const isSomeModalOpen = dayModal.open || exerciseModal.open;
+    const isSomeModalOpen =
+      (dayModal.open || exerciseModal.open) && !chooseExerciseModal.open;
     if (!modalChildren.includes(event.target) && isSomeModalOpen) {
       dayModal.close();
       exerciseModal.close();
