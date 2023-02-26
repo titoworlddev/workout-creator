8dc6bc8a37516ac1864f7b6041cd6fd5f642c9e1
titoworlddev <titocristian.dev@gmail.com>
2023-02-26 13:13:42 +0100

    Improved modalClickOutCloser fn

--- src/utils/functions/modalClickOutCloser.js
+++ src/utils/functions/modalClickOutCloser.js
@@ -1,20 +1,14 @@
 export function modalClickOutCloser() {
   window.removeEventListener('click', () => {});
 
-  const dayModal = document.querySelector('#day-modal');
-  const exerciseModal = document.querySelector('#exercise-modal');
-  const chooseExerciseModal = document.querySelector('#choose-exercise-modal');
+  const dayModal = document.querySelectorAll('dialog');
 
-  const modalChildren = [
-    ...Array.from(dayModal.querySelectorAll('*')),
-    ...Array.from(exerciseModal.querySelectorAll('*'))
-  ];
   window.addEventListener('click', event => {
-    const isSomeModalOpen =
-      (dayModal.open || exerciseModal.open) && !chooseExerciseModal.open;
-    if (!modalChildren.includes(event.target) && isSomeModalOpen) {
-      dayModal.close();
-      exerciseModal.close();
-    }
+    dayModal.forEach(modal => {
+      const children = Array.from(modal.querySelectorAll('*'));
+      if (!children.includes(event.target) && modal.open) {
+        modal.close();
+      }
+    });
   });
 }
