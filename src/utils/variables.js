0860c8b53df36b1ab0a854f3171cabd890b41a0c
titoworlddev <titocristian.dev@gmail.com>
2023-03-04 14:07:57 +0100

    removed useWorkoutInfo hook and moved to utils/variables.js

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,4 +1,7 @@
-export let exerciseIdTemp = '';
+export let temporals = {
+  exerciseIdTemp: '',
+  dayNameTempToChooseExercise: ''
+};
 
 export let workoutInfo = JSON.parse(localStorage.getItem('workoutInfo')) || {
   workoutName: '',
