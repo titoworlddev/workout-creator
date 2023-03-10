d9af0564d2ce92494b609e6291522cb6fe5edefa
titoworlddev <titocristian.dev@gmail.com>
2023-03-10 23:55:42 +0100

    Add alpha version message

--- src/pages/HomePage/components/HomeMain/HomeMain.jsx
+++ src/pages/HomePage/components/HomeMain/HomeMain.jsx
@@ -4,6 +4,15 @@ import HomeIntro from '../HomeIntro/HomeIntro';
 export default function HomeMain() {
   return (
     <main className="home-main">
+      <div className="alpha-version-message">
+        <p>
+          This is still the alpha version, it is not the final product, the
+          design will change and certain functionalities will also change, in
+          addition to others that will be added. If there is any operating
+          error, please excuse us, it will be fixed as soon as possible.
+        </p>
+      </div>
+      <br />
       <HomeIntro />
     </main>
   );
